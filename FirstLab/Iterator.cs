c827aa7f91980797b023d0caf844889e4fc04f61
Ihlaikin Mykola <ggg501434@gmail.com>
2024-12-29 23:16:47 +0200

    First commit

---
+++ FirstLab/Iterator.cs
@@ -0,0 +1,41 @@
+namespace FirstLab;
+
+public static class Iterator {
+    public static void MyAsyncMap<TEntry, TResult>(
+        TEntry[] entryArray,
+        Action<TEntry, Action<string?, TResult?>> asyncAction,
+        Action<List<string>, TResult[]> onCompleteCallback
+    ) {
+        var resultArray = new TResult[entryArray.Length];
+        var occuredErrors = new List<string>();
+        var processedElementsCounter = 0;
+        
+        for (var i = 0; i < entryArray.Length; i++) {
+            var currentElement = entryArray[i];
+            
+            asyncAction(
+                currentElement,
+                OnElementProcessed(i)
+            );
+            
+        }
+
+        return;
+        
+        Action<string?, TResult?> OnElementProcessed(int index) => (errorMassage, newElement) => {
+            processedElementsCounter++;
+
+            if (errorMassage is not null) occuredErrors.Add(errorMassage);
+            
+            else {
+                Console.WriteLine($"{entryArray[index]}, which has index {index} was handled");
+                resultArray[index] = newElement!;
+            }
+
+            var allElementsProcessed = processedElementsCounter == entryArray.Length;
+            if (allElementsProcessed) onCompleteCallback(occuredErrors, resultArray);
+            
+        };
+    }
+    
+} 
---
+++ FirstLab/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using FirstLab;
+
+var domainNames = new[] { "microsoft.com", "google.com" };
+
+Iterator.MyAsyncMap<string, string>(
+    domainNames,
+    GetHostEntryAsync,
+    PrintResults
+);
+
+Console.ReadKey();
+
+return;
+
+void PrintResults(List<string> errorMassages, string[] ipAddresses) {
+    if (errorMassages.Count != 0) {
+        foreach (var massage in errorMassages) {
+            Console.WriteLine(massage);
+        }
+    }
+
+    else {
+        Console.WriteLine();
+        
+        for (var i = 0; i < ipAddresses.Length; i++) {
+            var domainName = domainNames[i];
+            var ipAddress = ipAddresses[i];
+
+            var stringToPrint = $"{domainName} has such ip addresses:" +
+                                $"{Environment.NewLine}{ipAddress}" +
+                                Environment.NewLine;
+            
+            Console.WriteLine(stringToPrint);
+        }
+        
+    }
+    
+}
+
+void GetHostEntryAsync(string hostName, Action<string?, string?> callback) {
+    Dns.BeginGetHostEntry(hostName, asyncResult => {
+        try {
+            var hostEntry = Dns.EndGetHostEntry(asyncResult);
+            var ipAddresses = hostEntry.AddressList;
+
+            var formattedAddresses = string.Join(Environment.NewLine, ipAddresses.Select(
+                address => address.ToString()
+            ));
+
+            callback(null, formattedAddresses);
+        }
+        
+        catch (Exception ex) {
+            callback(ex.Message, null);
+        }
+        
+    }, null);
+}
