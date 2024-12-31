7458172dbd80b81ab5f842116352373ae7422bc1
Ihlaikin Mykola <ggg501434@gmail.com>
2024-12-31 21:08:32 +0200

    Added usage example

--- SecondLab/Program.cs
+++ SecondLab/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using SecondLab;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("App is running");
+
+var domainNames = new[] { "google.com", "github.com", "youtube.com" };
+
+domainNames.AsyncMap(Dns.GetHostAddressesAsync).ContinueWith(task => {
+    var ipAddresses = task.Result;
+
+    for (var i = 0; i < ipAddresses.Length; i++) {
+        var domainName = domainNames[i];
+        var ipAddress = ipAddresses[i];
+        
+        Console.WriteLine($"{domainName}, IP:{ipAddress.First()}");
+    }
+});
+
+Console.ReadKey();
