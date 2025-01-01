8c7bafdc73dd773a3a89fc88d1fa74248b3a5878
Ihlaikin Mykola <ggg501434@gmail.com>
2025-01-01 02:54:08 +0200

    Updated Program.cs

--- SecondLab/Program.cs
+++ SecondLab/Program.cs
@@ -3,17 +3,22 @@ using SecondLab;
 
 Console.WriteLine("App is running");
 
-var domainNames = new[] { "google.com", "github.com", "youtube.com" };
+var domainNames = new[] { "googssfle.com", "mifsfcrosoft.com", "jwfjf", "jfa;j" };
 
-domainNames.AsyncMap(Dns.GetHostAddressesAsync).ContinueWith(task => {
-    var ipAddresses = task.Result;
-
-    for (var i = 0; i < ipAddresses.Length; i++) {
-        var domainName = domainNames[i];
-        var ipAddress = ipAddresses[i];
-        
-        Console.WriteLine($"{domainName}, IP:{ipAddress.First()}");
-    }
-});
+domainNames.AsyncMap(Dns.GetHostAddressesAsync).ContinueWith(PrintResults);
 
 Console.ReadKey();
+
+return;
+
+void PrintResults(Task<(IPAddress[][], List<Exception>)> task) {
+    var (ipAddresses, exceptions) = task.Result;
+    
+    if(exceptions.Count != 0) exceptions.ForEach(Console.WriteLine);
+    else {
+        foreach (var ip in ipAddresses) {
+            Console.WriteLine($"IP:{ip.First()}");
+        }
+    }
+
+}
