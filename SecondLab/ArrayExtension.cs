714596631a8f182ebdde6a4e8f4e438bcf5f991c
Ihlaikin Mykola <ggg501434@gmail.com>
2025-01-01 02:53:37 +0200

    Updated AsyncMap

--- SecondLab/ArrayExtension.cs
+++ SecondLab/ArrayExtension.cs
@@ -2,20 +2,20 @@ namespace SecondLab;
 
 public static class ArrayExtension {
     
-    public static Task<TOutput[]> AsyncMap<TInput, TOutput>(
+    public static Task<(TOutput[], List<Exception>)> AsyncMap<TInput, TOutput>(
         this TInput[] inputArray,
         Func<TInput, Task<TOutput>> getAsyncTask
     ) {
         var arraysLength = inputArray.Length;
-        
+
         var outputArray = new TOutput[arraysLength];
         var occuredExceptions = new List<Exception>();
         var tasks = new List<Task>();
-        
+
         for (var i = 0; i < arraysLength; i++) {
             var capturedIndex = i;
             var currentElement = inputArray[capturedIndex];
-            
+
             var task = getAsyncTask(currentElement).ContinueWith(resultTask => {
                 if (resultTask.IsFaulted) {
                     var exceptions = resultTask.Exception.InnerExceptions;
@@ -27,15 +27,9 @@ public static class ArrayExtension {
             });
 
             tasks.Add(task);
-            
         }
-
-        var resultTask = Task.WhenAll(tasks).ContinueWith(_ => {
-            if (occuredExceptions.Count != 0) throw new AggregateException(occuredExceptions);
-            return outputArray;
-        });
-
-        return resultTask;
+        
+        return Task.WhenAll(tasks).ContinueWith(_ => (outputArray, occuredExceptions));
     }
     
 }
